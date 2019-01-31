0d9488832b8bf389e41fe963937f8e4fee842237
Dylanloic <dylanschito@gmail.com>
2019-01-31 14:00:46 +0100

    Add .js scripts

---
+++ ToolsImages.js
@@ -0,0 +1,21 @@
+
+class ToolsImages{
+
+    Photomaton(nb = 2, action='do'){
+        var nbx = Array.isArray(nb) ? nb[0] : nb;
+        var nby = Array.isArray(nb) ? nb[1] : nb;
+
+        var width = 256;
+        var height = 256;
+
+        width -= width%nbx;
+        height -= height%nby;
+
+        var copy = createImage(width, height, RGB);
+        
+        
+
+        
+
+    }
+}
