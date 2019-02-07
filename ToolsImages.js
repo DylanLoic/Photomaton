9a4efc9c6ba34761a36000e1f9d553b83e2f27e6
Dylanloic <dylanschito@gmail.com>
2019-02-07 22:06:40 +0100

    inX, couplage transformation done

--- ToolsImages.js
+++ ToolsImages.js
@@ -12,17 +12,7 @@ class ToolsImages{
         this.Height = height;
     }
 
-    Photomaton(nb = 2, action='do'){
-        var nbx = Array.isArray(nb) ? nb[0] : nb;
-        var nby = Array.isArray(nb) ? nb[1] : nb;
-
-        var width = 256;
-        var height = 256;
-
-        width -= width%nbx;
-        height -= height%nby;
-
-        var copy = createImage(width, height, RGB);
+    Photomaton(){
         
 
     }
