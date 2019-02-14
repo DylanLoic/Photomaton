d0a3625f8c5debb88a9eabddac65ebd1afa7273d
Flowfly <mudespacher.florian@gmail.com>
2019-02-14 14:14:37 +0100

    Updated index to make it more beautiful :)

--- ToolsImages.js
+++ ToolsImages.js
@@ -1,5 +1,18 @@
 
 class ToolsImages{
+    
+    /*width = 0;
+    height = 0;
+    canvas;
+    image;*/
+
+    constructor(pImage, pCanvas)
+    {
+        this.image = pImage;
+        this.canvas = pCanvas;
+    }
+
+    
 
     Photomaton(nb = 2, action='do'){
         var nbx = Array.isArray(nb) ? nb[0] : nb;
@@ -38,7 +51,25 @@ class ToolsImages{
 
     InX()
     {
-
+        var imagedata = this.canvas.getImageData(0,0,this.canvas.canvas.width, this.canvas.canvas.height);
+        var tmpimagedata = imagedata;
+        console.log(imagedata);
+        for(var i = 0; i < this.canvas.canvas.width; i++)
+        {
+            for(var j = 0; j < this.canvas.canvas.height; j++)
+            {
+                if(i % 2 == 0)
+                {
+                    tmpimagedata.data[i + 2] = imagedata.data[i];
+                }
+                else
+                {
+                    tmpimagedata.data[i - 2] = imagedata.data[i];
+                }
+            }
+        }
+        console.log(tmpimagedata);
+        //console.log("Largeur = " + this.canvas.canvas.width + " \nHauteur = " + this.canvas.canvas.height);
     }
 
 }
